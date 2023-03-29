47c99bbec99873a6647a169c772dca416adc5204
chenjunqian <chenjunqian0810@foxmail.com>
2023-03-29 21:19:16 +0800

    update docker log-opt

--- mysql-rssgo/start_container.sh
+++ mysql-rssgo/start_container.sh
@@ -1,1 +1,1 @@
-docker run --name guoshao-db --log-opt max-size=10m --log-opt max-file=3 -p 3306:3306 -d beegedelow/guoshao-mysql
+docker run --name guoshao-db --log-opt max-size=500m --log-opt max-file=3 -p 3306:3306 -d beegedelow/guoshao-mysql
