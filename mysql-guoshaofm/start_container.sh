27c94b7296e8660a303909a4118a153d798d3a92
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-29 23:06:12 +0800

    refactor: mount data volumn

--- mysql-guoshaofm/start_container.sh
+++ mysql-guoshaofm/start_container.sh
@@ -1,1 +1,7 @@
-docker run --name guoshaofm-mariadb -v /my/own/datadir:/var/lib/mysql -e MARIADB_ROOT_PASSWORD=my-secret-pw -d mariadb:latest
+DB_DATA_DIR=$HOME/guoshaofm/mariadb/datadir
+if [[ ! -e $DB_DATA_DIR ]]; then
+    mkdir -p $DB_DATA_DIR
+elif [[ ! -d $DB_DATA_DIR ]]; then
+    echo "$DB_DATA_DIR already exists but is not a directory" 1>&2
+fi
+docker run --name guoshaofm-mariadb -v $DB_DATA_DIR:/var/lib/mysql -d mariadb:latest
