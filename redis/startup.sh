547c65709181faddef9ca63d054e6d4f5e54da66
chenjunqian <chenjunqian0810@foxmail.com>
2023-08-28 22:36:48 +0800

    feat: add redis docker startup script

---
+++ redis/startup.sh
@@ -0,0 +1,12 @@
+REDIS_WORKSPACE=$HOME/guoshao-redis
+if [[ ! -e $REDIS_WORKSPACE ]]; then
+    mkdir -p $REDIS_WORKSPACE
+elif [[ ! -d $REDIS_WORKSPACE ]]; then
+    echo "$REDIS_WORKSPACE already exists but is not a directory" 1>&2
+fi
+
+docker run -v $REDIS_WORKSPACE/data:/data \
+    -v $REDIS_WORKSPACE/logs:/var/log/redis/
+    -p 6379:6379 \
+    --name guoshaofm-redis \
+    -d redis:latest --requirepass 1qaz!QAZ
