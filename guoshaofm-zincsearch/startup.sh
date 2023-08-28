57a5cd3770d4036bb1e590596a2042fbb7ece9a0
chenjunqian <chenjunqian0810@foxmail.com>
2023-08-28 22:37:05 +0800

    feat: add zincsearch startup script

---
+++ guoshaofm-zincsearch/startup.sh
@@ -0,0 +1,13 @@
+DB_DATA_DIR=$HOME/guoshaofm/zincsearch/data
+if [[ ! -e $DB_DATA_DIR ]]; then
+    mkdir -p $DB_DATA_DIR
+elif [[ ! -d $DB_DATA_DIR ]]; then
+    echo "$DB_DATA_DIR already exists but is not a directory" 1>&2
+fi
+
+
+docker run -v $DB_DATA_DIR:/data -e ZINC_DATA_PATH="/data" -p 4080:4080 \
+    -e ZINC_FIRST_ADMIN_USER=admin -e ZINC_FIRST_ADMIN_PASSWORD=qazxsw \
+    -e ZINC_PLUGIN_GSE_ENABLE=true -e ZINC_PLUGIN_GSE_DICT_EMBED=big \
+    --name guoshaofm-zincsearch public.ecr.aws/zinclabs/zincsearch:0.4.6
+
