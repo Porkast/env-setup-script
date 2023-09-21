897a4f018c1ac6ad22cacf08cc85c4b1a09155a1
chenjunqian <chenjunqian0810@foxmail.com>
2023-09-22 01:36:42 +0800

    update mysql script

--- guoshaofm-mysql/start_container.sh
+++ guoshaofm-mysql/start_container.sh
@@ -13,6 +13,13 @@ elif [[ ! -d $DATA_DIR ]]; then
     echo "$DATA_DIR already exists but is not a directory" 1>&2
 fi
 
+DB_CONFIG_DIR=$DB_HOME/config
+if [[ ! -e $DB_CONFIG_DIR ]]; then
+    mkdir -p $DB_CONFIG_DIR
+elif [[ ! -d $DB_CONFIG_DIR ]]; then
+    echo "$DB_CONFIG_DIR already exists but is not a directory" 1>&2
+fi
+
 echo "run guoshaofm-mysqldb container"
 
-docker run --name guoshaofm-mysqldb -p 3306:3306  -v $LOGS_DIR:/app/logs -v $DATA_DIR:/var/lib/mysql -d beegedelow/guoshaofm-mysqldb:latest
+docker run --name guoshaofm-mysqldb -p 3306:3306 -v $DB_CONFIG_DIR:/etc/mysql/conf.d -v $LOGS_DIR:/app/logs -v $DATA_DIR:/var/lib/mysql -d beegedelow/guoshaofm-mysqldb:latest
